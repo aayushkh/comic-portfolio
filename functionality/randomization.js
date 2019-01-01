2cc41f7035809da0f815401bc89e324b22c8e461
aayushkh <aayushkh@usc.edu>
2018-12-31 16:08:27 -0800

    the last stretch ?

--- functionality/randomization.js
+++ functionality/randomization.js
@@ -1,11 +1,24 @@
-(function() {
-    var bgArray = ['bluebg', 'greybg', 'redbg', 'yellowbg', 'orangebg', 'pinkbg', 'greenbg'];
-    var bg = bgArray[Math.floor(Math.random() * bgArray.length)];
-    document.querySelector(".about.panel").classList.add(bg);
-})();
+// (() => {
+//     var bgArray = ['bluebg', 'greybg', 'redbg', 'yellowbg', 'orangebg', 'pinkbg', 'greenbg'];
+//     var bg = bgArray[Math.floor(Math.random() * bgArray.length)];
+//     document.querySelector(".about.panel").classList.add(bg);
+// })();
 
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector(".navOption .navItem").addEventListener('click',function () {
-        console.log("hello");
+document.querySelectorAll(".navOption").forEach(option => {
+    let navOption = "." + option.dataset.nav;
+    option.addEventListener('click', () => {
+        let open = document.getElementsByClassName("open");
+        open[0].classList.add("closed");
+        open[0].classList.remove("open");
+        document.querySelector(navOption).classList.add("open");
+    });
+});
+
+document.querySelectorAll(".back-arrow").forEach(back => {
+    back.addEventListener('click', () => {
+        let open = document.getElementsByClassName("open");
+        open[0].classList.add("closed");
+        open[0].classList.remove("open");
+        document.querySelector(".home").classList.add("open");
     });
 });
