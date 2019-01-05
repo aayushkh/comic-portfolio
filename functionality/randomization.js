dc6e80643a1b206941a334fd58a8257563abf24f
aayushkh <aayushkh@usc.edu>
2019-01-05 11:12:56 -0800

    going strong !

--- functionality/randomization.js
+++ functionality/randomization.js
@@ -6,26 +6,20 @@
 
 var siteW = window.innerWidth;
 var siteH = window.innerHeight;
+var timeLine = new TimelineMax();
 
 TweenMax.set(".site", { perspective: 5000 });
 TweenMax.set(".container", { transformStyle: "preserve-3d",  transformOrigin: '-0% 50%' });
-TweenMax.set(".closed", { rotationY: 90, z: -siteW/2, x: siteW/2 });
-
-var timeLine = new TimelineMax();
+TweenMax.set("aside", { rotationY: 90, z: -siteW/2, x: siteW/2 });
 
 document.querySelectorAll(".navOption").forEach(option => {
     let navOption = "." + option.dataset.nav;
+
     option.addEventListener('click', () => {
         let open = document.getElementsByClassName("open");
 
-        open[0].classList.add("closed");
         open[0].classList.remove("open");
         document.querySelector(navOption).classList.add("open");
-
-        // timeLine
-        // .to('.site', .5, { scale: 0.75, rotationZ: -5, ease: Power4.easeInOut }, "start")
-        // .to('.container', .4, { rotationY: -90, z: -siteW, ease: Power4.easeInOut }, "start+=0.7")
-        // .to('.site', .5, { rotationZ: 0, scale: 1, ease: Power4.easeInOut }, "start+=1.2");
 
         timeLine
         .to('.site', .5, { scale: 0.75, ease: Power4.easeInOut }, "start")
@@ -36,12 +30,6 @@ document.querySelectorAll(".navOption").forEach(option => {
 
 document.querySelectorAll(".back-arrow").forEach(back => {
     back.addEventListener('click', () => {
-        let open = document.getElementsByClassName("open");
-
-        open[0].classList.add("closed");
-        document.querySelector(".home").classList.add("open");
-        open[0].classList.remove("open");
-
         timeLine.reverse();
         timeLine = new TimelineMax();
     });
